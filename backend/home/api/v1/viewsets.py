bdb0cf09b86e7989d96a62db2d0034502da70e0d
Crowdbotics <team@crowdbotics.com>
2021-01-13 16:30:31 +0000

    Update models

--- backend/home/api/v1/viewsets.py
+++ backend/home/api/v1/viewsets.py
@@ -1,3 +1,11 @@
+from rest_framework import viewsets
+from rest_framework import authentication
+from .serializers import (
+    CustomTextSerializer,
+    EventsSerializer,
+    HomePageSerializer,
+    LocationSerializer,
+)
 from rest_framework.authentication import SessionAuthentication, TokenAuthentication
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.permissions import IsAdminUser
@@ -11,7 +19,7 @@ from home.api.v1.serializers import (
     HomePageSerializer,
     UserSerializer,
 )
-from home.models import CustomText, HomePage
+from home.models import CustomText, Events, HomePage, Location
 
 
 class SignupViewSet(ModelViewSet):
@@ -49,3 +57,21 @@ class HomePageViewSet(ModelViewSet):
     authentication_classes = (SessionAuthentication, TokenAuthentication)
     permission_classes = [IsAdminUser]
     http_method_names = ["get", "put", "patch"]
+
+
+class EventsViewSet(viewsets.ModelViewSet):
+    serializer_class = EventsSerializer
+    authentication_classes = (
+        authentication.SessionAuthentication,
+        authentication.TokenAuthentication,
+    )
+    queryset = Events.objects.all()
+
+
+class LocationViewSet(viewsets.ModelViewSet):
+    serializer_class = LocationSerializer
+    authentication_classes = (
+        authentication.SessionAuthentication,
+        authentication.TokenAuthentication,
+    )
+    queryset = Location.objects.all()
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
@@ -12,18 +13,31 @@ class CustomText(models.Model):
     app from the project.
     """
 
-    title = models.CharField(max_length=150)
+    title = models.CharField(
+        max_length=150,
+    )
 
     def __str__(self):
         return self.title
 
     @property
     def api(self):
-        return f'/api/v1/customtext/{self.id}/'
+        return f"/api/v1/customtext/{self.id}/"
 
     @property
     def field(self):
-        return 'title'
+        return "title"
+
+    def __str__(self):
+        return self.title
+
+    @property
+    def api(self):
+        return f"/api/v1/customtext/{self.id}/"
+
+    @property
+    def field(self):
+        return "title"
 
 
 class HomePage(models.Model):
@@ -32,12 +46,49 @@ class HomePage(models.Model):
     successfuly built. All references to it should be removed in order to remove this
     app from the project.
     """
+
     body = models.TextField()
 
     @property
     def api(self):
-        return f'/api/v1/homepage/{self.id}/'
+        return f"/api/v1/homepage/{self.id}/"
 
     @property
     def field(self):
-        return 'body'
+        return "body"
+
+    @property
+    def api(self):
+        return f"/api/v1/homepage/{self.id}/"
+
+    @property
+    def field(self):
+        return "body"
+
+
+class Events(models.Model):
+    "Generated Model"
+    name = models.CharField(
+        max_length=256,
+    )
+    date = models.DateField()
+    location = models.ForeignKey(
+        "home.Location",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="events_location",
+    )
+
+
+class Location(models.Model):
+    "Generated Model"
+    street = models.CharField(
+        max_length=256,
+    )
+    city = models.CharField(
+        max_length=256,
+    )
+    state = models.CharField(
+        max_length=256,
+    )
